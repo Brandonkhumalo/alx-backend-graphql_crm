acb3a27c4be44f489ba2830fd0c38025aaa3fd2d
Brandon Khumalo <127394576+Brandonkhumalo@users.noreply.github.com>
2025-07-10 21:45:45 +0200

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -3,3 +3,9 @@ INSTALLED_APPS = [
     'django_crontab',
     'crm',  # your app
 ]
+
+CRONJOBS = [
+    ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
+    ('0 8 * * *', 'crm.cron.send_order_reminders'),
+    ('0 */12 * * *', 'crm.cron.update_low_stock'),
+]
