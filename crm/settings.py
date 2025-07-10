added03a51ed58342db58cc381672bccd5c498e1
Brandon Khumalo <127394576+Brandonkhumalo@users.noreply.github.com>
2025-07-10 21:46:16 +0200

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -1,3 +1,12 @@
+from celery.schedules import crontab
+
+CELERY_BEAT_SCHEDULE = {
+    'generate-crm-report': {
+        'task': 'crm.tasks.generate_crm_report',
+        'schedule': crontab(day_of_week='mon', hour=6, minute=0),
+    },
+}
+
 INSTALLED_APPS = [
     # other apps
     'django_crontab',
